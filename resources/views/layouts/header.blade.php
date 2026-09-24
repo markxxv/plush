d3ce2e3683f8a99db4b4a33e8b6596dadf8f07e7
markxxv <mbogrov@gmail.com>
2026-09-24 19:24:31 +0200

    Add accessible names to header actions

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,7 +11,12 @@
             >
                 <x-tabler-search class="h-4 w-4" stroke-width="1.2" />
             </button>
-            <a x-data href="{{ localized_route('wishlist') }}" class="flex items-center gap-2">
+            <a
+                x-data
+                href="{{ localized_route('wishlist') }}"
+                class="flex items-center gap-2"
+                aria-label="{{ __('Wishlist') }}"
+            >
                 <x-tabler-heart class="w-4 h-4"  stroke-width="1.2"  />
                 <div x-show="$store.wishlist.items.length > 0" x-text="$store.wishlist.items.length" class="text-sm">0</div>
             </a>
@@ -34,7 +39,12 @@
 {{--                </button>--}}
 {{--            </div>--}}
 
-            <button  @click="$store.cart.toggleCart()" class="group relative p-1.5 rounded-full transition-colors duration-300 cursor-pointer">
+            <button
+                type="button"
+                @click="$store.cart.toggleCart()"
+                class="group relative p-1.5 rounded-full transition-colors duration-300 cursor-pointer"
+                aria-label="{{ __('Open cart') }}"
+            >
                     <svg class="w-4 h-4 transition-colors duration-300"
                          fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
